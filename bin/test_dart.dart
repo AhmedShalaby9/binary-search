706b39e086b6e5dbe712408f99e2b93c888e7917
Ahmed gamal shalaby <shalabi.dev@gmail.com>
2022-08-18 17:41:24 +0200

    ...

---
+++ bin/test_dart.dart
@@ -0,0 +1,23 @@
+void main() {
+  // binary search
+  List<int> list = [2, 4, 6, 8, 9, 10, 18];
+  print(binarySearch(list, 6));
+}
+
+String binarySearch(List<int> list, x) {
+  int min = 0;
+  int max = list.length - 1;
+  while (min <= max) {
+    int mid = ((min + max) / 2).floor();
+    if (x == list[mid]) {
+      return "selected item found in index $mid";
+    } else if (x > list[mid]) {
+      min = mid + 1;
+    } else if (x < list[mid]) {
+      max = mid - 1;
+    } else {
+      return "selected item found in index $mid";
+    }
+  }
+  return "not found";
+}
